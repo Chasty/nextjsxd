3f16f09b51ad2b4487aea3b2bf25a28d3abba48c
Willy <wrosahua@everis.com>
2020-12-11 14:49:53 -0500

    nuevo cambiosxd

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Willy Rosa Huanca.js!</a>
+          Welcome to <a href="https://nextjs.org">Willy Cesitar XD Rosa Huanca.js!</a>
         </h1>
 
         <p className={styles.description}>
